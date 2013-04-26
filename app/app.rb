8aff2580fad5c797c44359f1df8aaf7b4e601007
Tim Shelburne <shelburt02@gmail.com>
2013-04-26 16:11:39 -0500

    updated app.rb to be caught up with mock responses for syncing

--- app/app.rb
+++ app/app.rb
@@ -3,10 +3,6 @@ class SyncApp < Sinatra::Base
 
 	get '/test' do
 		@command_source = Commands::CommandSource.new("1234")
-		@external_sources = [
-			Commands::CommandSource.new("2345"),
-			Commands::CommandSource.new("3456")
-		]
 		haml :test
 	end
 
@@ -21,6 +17,10 @@ class SyncApp < Sinatra::Base
 					{ name: 'log', data: { message: 'received command'}, timestamp: 2 },
 					{ name: 'increment', data: { }, timestamp: 1 },
 					{ name: 'increment', data: { }, timestamp: 3 }
+				],
+				currentSources: [
+					{ id: "2345", lastUpdate: 2 },
+					{ id: "3456", lastUpdate: 6 }
 				],
 				stableTimestamp: 1
 			}
