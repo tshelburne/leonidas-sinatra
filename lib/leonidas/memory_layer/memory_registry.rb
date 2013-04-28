d532a5a9d5cb824f036392f2f19629707bc693cc
Tim Shelburne <shelburt02@gmail.com>
2013-04-28 12:43:38 -0500

    updated memory registry to prevent adding two apps with the same name

--- lib/leonidas/memory_layer/memory_registry.rb
+++ lib/leonidas/memory_layer/memory_registry.rb
@@ -7,6 +7,7 @@ module Leonidas
 
 			def self.register_app!(app)
 				raise TypeError, "Argument must include Leonidas::App::App" unless app.class < Leonidas::App::App
+				raise StandardError, "An app with the name '#{app.name}' is already registered" if Leonidas::MemoryLayer::MemoryRegistry.has_app? app.name
 				@@apps[app.name] = app
 			end
 
@@ -14,7 +15,7 @@ module Leonidas
 				@@apps[name]
 			end
 
-			def self.has_app_registered?(name)
+			def self.has_app?(name)
 				not MemoryRegistry.retrieve_app(name).nil?
 			end
 
