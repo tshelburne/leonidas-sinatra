b30d68334820e3dcebc9bd40330b3f0b9ad44814
Tim Shelburne <shelburt02@gmail.com>
2013-06-01 01:34:25 -0500

    added marker to show when a command has been run to the command class

--- lib/leonidas/commands/command.rb
+++ lib/leonidas/commands/command.rb
@@ -11,10 +11,23 @@ module Leonidas
 				@data = data
 				@client_id = client_id
 				@timestamp = timestamp
+				@has_run = false
 			end
 
 			def to_hash
 				{ id: @id, name: @name, data: @data, clientId: @client_id, timestamp: @timestamp.as_milliseconds }
+			end
+
+			def has_run?
+				@has_run
+			end
+
+			def mark_as_run!
+				@has_run = true
+			end
+
+			def mark_as_not_run!
+				@has_run = false
 			end
 
 		end
--- spec/rspec/unit/commands/command_spec.rb
+++ spec/rspec/unit/commands/command_spec.rb
@@ -16,4 +16,42 @@ describe Leonidas::Commands::Command do
 
 	end
 
+	describe '#has_run?' do
+		
+		it "will default to false" do
+			subject.should_not have_run
+		end
+
+		it "will return true when the command is marked as run" do
+			subject.mark_as_run!
+			subject.should have_run
+		end
+
+		it "will return false when the command is marked as not run" do
+			subject.mark_as_run!
+			subject.mark_as_not_run!
+			subject.should_not have_run
+		end
+
+	end
+
+	describe '#mark_as_run!' do 
+	
+		it "will mark the command as having been run" do
+			subject.mark_as_run!
+			subject.should have_run
+		end
+	
+	end
+
+	describe '#mark_as_not_run!' do 
+	
+		it "will mark the command as not having been run" do
+			subject.mark_as_run!
+			subject.mark_as_not_run!
+			subject.should_not have_run
+		end
+	
+	end
+
 end
